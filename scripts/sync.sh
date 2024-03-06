a24cd42b4e0dd99f964dfcf3f6b6251335b9b906
Trent Johnson <trentj@netflixcontractors.com>
2024-03-05 22:37:32 -0800

    cover sync update

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-rsync -e ssh --rsync-path="sudo rsync" -rpt --delete --progress /home/trentj/web/personal/library-server/mediabin/files/covers trentj@192.168.1.200:/home/trentj/web/library-server/mediabin/files
+# pi server -> local
+rsync -e ssh --rsync-path="sudo rsync" -rpt --delete --progress trentj@192.168.1.200:/home/trentj/web/library-server/mediabin/files/covers /home/trentj/web/personal/library-server/mediabin/files
