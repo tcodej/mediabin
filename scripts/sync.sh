f4c6ea312b924cb4002ccafea024ab6b305686e9
Trent Johnson <trentj@netflixcontractors.com>
2025-06-19 10:02:41 -0700

    upgrade packages

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # pi server -> local
-rsync -e ssh --rsync-path="sudo rsync" -rpt --delete --progress trentj@192.168.1.200:/home/trentj/web/library-server/mediabin/files/covers /home/trentj/web/personal/library-server/mediabin/files
+rsync -e ssh -rpt --delete --progress trentj@192.168.1.200:/home/trentj/web/library-server/mediabin/files/covers /home/trentj/web/personal/library-server/mediabin/files
